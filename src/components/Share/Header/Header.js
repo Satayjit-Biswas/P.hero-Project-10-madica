814767aa392ca9d50f3355d744f5c5272b74778b
Satayjit Biswas <61390425+Satayjit-Biswas@users.noreply.github.com>
2021-10-19 22:49:06 +0600

    v8

--- src/components/Share/Header/Header.js
+++ src/components/Share/Header/Header.js
@@ -22,7 +22,7 @@ const Header = () => {
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <NavLink to='/'>Home</NavLink>
+                                <NavLink exact to='/'>Home</NavLink>
                             </li>
                             <li className="nav-item">
                                 <NavLink to='/about'>About</NavLink>
